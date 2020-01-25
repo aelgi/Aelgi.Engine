831bca401be51cea207af07dc7c1b6566d8cf9ce
sanchez <dfitz360@gmail.com>
2020-01-25 14:40:15 +1000

    able to load appsettings at runtime from configuration

---
+++ Aelgi.Engine.Server/AppSettings.fs
@@ -0,0 +1,9 @@
+﻿namespace Aelgi.Engine.Server
+
+type NeoSettings () =
+    member val URI = "" with get, set
+    member val Username = "" with get, set
+    member val Password = "" with get, set
+
+type AppSettings () =
+    member val Neo4j = NeoSettings () with get, set
--- Aelgi.Engine.Server/Program.fs
+++ Aelgi.Engine.Server/Program.fs
@@ -1,10 +1,41 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open Aelgi.Engine.Server
+open System
 open Aelgi.Engine.Server.Handler
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+let configureAppConfiguration (context: HostBuilderContext) (config: IConfigurationBuilder) =
+    let environmentAppSettings =
+        context.HostingEnvironment.EnvironmentName
+        |> sprintf "appsettings.%s.json"
+    
+    config
+        .AddJsonFile("appsettings.json", false, true)
+        .AddJsonFile(environmentAppSettings, true, true)
+        .AddEnvironmentVariables() |> ignore
+        
+    ()
+    
+let configureServices (context: HostBuilderContext) (services: IServiceCollection) =
+    let appSettings = AppSettings()
+    context.Configuration.GetSection("App").Bind(appSettings)
+    
+    services.AddSingleton(appSettings) |> ignore
+    
+    ()
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let builder =
+        HostBuilder()
+            .ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> configureAppConfiguration)
+            .ConfigureServices(Action<HostBuilderContext, IServiceCollection> configureServices)
+            .Build()
+
+    let appSettings = builder.Services.GetService<AppSettings>()         
 
     Server.listen 7707
 
